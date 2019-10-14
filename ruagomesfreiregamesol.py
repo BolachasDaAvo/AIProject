6b6be9f5bb735caacc4082b0db58163e71aee691
BolachasDaAvo <miguelscfneves@tecnico.ulisboa.pt>
2019-10-14 22:24:36 +0100

    did some stuff

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -1,6 +1,6 @@
 import math
 import pickle
-import time        
+import time
 
 class Node:
 
@@ -28,20 +28,24 @@ class SearchProblem:
         sol = []
         openNodes = []
         closedNodes = []
-        gn = 0
-        hn = 0
-
+        exp = limitexp
+        depth = limitdepth
+        g = 0
+        h = 0
+        d = 0
         goal = self.goal[0]
+        baseNode = Node(0, 0, 0, 0, [])
 
         for n in init:
-            gn = 0
-            hn = h(self.auxheur[n - 1], self.auxheur[goal - 1])
-            startNode = Node(n, -1, gn, hn, tickets) 
+            g = 0
+            d = distance(self.auxheur[n - 1], self.auxheur[goal - 1])
+            h = 1
+            startNode = Node(n, baseNode, 0, h, tickets) 
             openNodes += [startNode]
 
         while openNodes != []:
 
-            #Gets node with greater f
+            #Gets node with least f
             q = openNodes[0]
             for x in openNodes:
                 if x.f < q.f:
@@ -53,50 +57,57 @@ class SearchProblem:
                 transp = x[0]                
                 n = x[1]
 
-                gn = q.g + g(self.auxheur[q.n - 1], self.auxheur[n - 1])
-                hn = h(self.auxheur[n - 1], self.auxheur[goal - 1])
-                newNode = Node(n, q, gn, hn, q.tickets)
+                #Node is parent node
+                if n == q.p.n:
+                    continue
 
-                if newNode.n == goal:
+                if n == goal:
 
                     #Trace path
-                    node = newNode
-                    while node.p != -1:
+                    sol += [n]
+                    node = q
+                    while node.n != 0:
                         sol += [node.n]
                         node = node.p
-                    sol += [node.n]
                     sol.reverse()
                     return sol
+
+                g = q.g + 1
+                h = distance(self.auxheur[n - 1], self.auxheur[goal - 1]) / d
+                newNode = Node(n, q, g, h, [*(q.tickets)])
 
                 #We dont have enough tickets to travel
                 if newNode.remTicket(transp) < 0:
                     continue
 
-                #Check if node is already in openNodes list and has a greater f
+                #Check if node is already in openNodes list and has a lower f
                 flag = 0
                 for x in openNodes:
-                    if x.n == newNode.n and x.f < newNode.f:
+                    if x.n == newNode.n and x.tickets == newNode.tickets and x.f < newNode.f:
                         flag = 1
                         break
                 if (flag):
                     continue
 
-                #Check if node is in closed list and and has a greater h
+                #Check if node is in closed list and and has a lower f
                 flag = 0
                 for x in closedNodes:
-                    if x.n == newNode.n and x.f < newNode.f:
+                    if x.n == newNode.n and x.tickets == newNode.tickets and x.f < newNode.f:
                         flag = 1
                         break
                 if (flag):
                     continue
                 openNodes += [newNode]
+                exp -= 1
+                if exp < 0:
+                    return []
+
 
             closedNodes += [q]
 
         return []
 
-h = lambda t1, t2 : math.sqrt((t2[0] - t1[0])**2 + (t2[1] - t1[1])**2)
-g = lambda t1, t2 : math.sqrt((t2[0] - t1[0])**2 + (t2[1] - t1[1])**2)
+distance = lambda t1, t2 : math.sqrt((t2[0] - t1[0])**2 + (t2[1] - t1[1])**2)
 
 with open("coords.pickle", "rb") as fp:   # Unpickling
     coords = pickle.load(fp)
@@ -105,6 +116,13 @@ with open("mapasgraph.pickle", "rb") as fp:  # Unpickling
     AA = pickle.load(fp)
 M = AA[1]
 
-SP = SearchProblem([63], M, coords)
-SOL = SP.search([30])
+tinit = time.process_time()
+
+#Taxi = linhas brancas; Autocarro = linhas azuis; Metro = linhas vermelhas
+SP = SearchProblem([113], M, coords)
+SOL = SP.search([18], tickets = [5, 5, 2])
 print(SOL)
+
+tend = time.process_time()
+
+print(tend - tinit)
