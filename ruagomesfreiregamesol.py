e89e0d02dac05c4047ed7e8f392001d348d4cf9e
BolachasDaAvo <miguelscfneves@tecnico.ulisboa.pt>
2019-10-19 15:46:58 +0100

    more cool

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -1,6 +1,8 @@
 import math
 import pickle
 import time
+import copy
+import matplotlib.pyplot as plt
 
 class Node:
 
@@ -57,11 +59,19 @@ class SearchProblem:
 
                 #Trace path
                 node = q
-                while node.n != 0:
-                    sol += [node.n]
+                while node.p.n != 0:
+                    # Find Used ticket
+                    usedTicket = 0
+                    ticketsp = node.p.tickets
+                    for i in range(3):
+                        if (ticketsp[i] > node.tickets[i]):
+                            usedTicket = i
+                            break
+                    sol += [[[i], [node.n]]]
                     node = node.p
+                sol += [[[], [node.n]]]
                 sol.reverse()
-                print("foram gerados %d nos" % len(openNodes))
+                ##print("foram gerados %d nos" % len(openNodes))
                 return sol
 
             exp -= 1
@@ -79,7 +89,7 @@ class SearchProblem:
                    continue
 
                 #We dont have enough tickets to travel
-                if tickets[transp] < 1:
+                if q.tickets[transp] < 1:
                     continue
 
                 tickets = [*(q.tickets)]
@@ -87,37 +97,15 @@ class SearchProblem:
 
                 g = q.g + 1
 
-                #Discovers what type of station it is
+                # Discovers what type of station it is
                 station = 0
                 for x in self.model[n]:
                     if x[0] > station and tickets[x[0]] > 1:
                         station = x[0]
 
-                h = distance(self.auxheur[n - 1], self.auxheur[goal - 1]) / averageCost[station]
+                h = math.floor(distance(self.auxheur[n - 1], self.auxheur[goal - 1]) / averageCost[station])
 
                 newNode = Node(n, q, g, h, tickets)
-
-                #Check if node is already in openNodes list and has a lower f
-                flag = 0
-                for x in openNodes:
-                    if x.n == newNode.n and x.f < newNode.f:
-                        if x.tickets[0] <= newNode.tickets[0] and x.tickets[1] <= newNode.tickets[1] and x.tickets[2] <= newNode.tickets[2]:
-                            flag = 1
-                            break
-                if (flag):
-                    print("skipped node %d" % newNode.n)
-                    continue
-
-                #Check if node is in closed list and has a lower f
-                flag = 0
-                for x in closedNodes:
-                    if x.n == newNode.n and x.f < newNode.f:
-                        if x.tickets[0] <= newNode.tickets[0] and x.tickets[1] <= newNode.tickets[1] and x.tickets[2] <= newNode.tickets[2]:
-                            flag = 1
-                            break
-                if (flag):
-                    print("skipped node %d" % newNode.n)
-                    continue
                 openNodes += [newNode]
 
         return []
@@ -164,6 +152,63 @@ def calcAverageCostTransp(coords, map):
 
     return averageCost
 
+def plotpath(P,coords):   
+        img = plt.imread('maps.png')
+        plt.imshow(img)
+        colors = ['r.-','g+-','b^-']
+        I = P[0][1]
+        for agind in range(len(P[0][1])):
+                st = I[agind]-1
+                for tt in P:                        
+                        nst = tt[1][agind]-1
+                        plt.plot([coords[st][0],coords[nst][0]],[coords[st][1],coords[nst][1]],colors[agind])
+                        st = nst
+        plt.axis('off')
+        fig = plt.gcf()
+        fig.set_size_inches(1.*18.5, 1.*10.5)
+        fig.savefig('test2png.png', dpi=100)   
+        plt.show()
+        
+def validatepath(oP,oI,U,tickets=[25,25,25]): 
+        print(oP)
+        if not oP:
+                return False
+        P = copy.deepcopy(oP)
+        I = copy.copy(oI)
+        mtickets = copy.copy(tickets)
+
+        print(I)
+        print(P[0][1])
+        if I!=P[0][1]:
+                print('path does not start in the initial state')
+                return False
+        del P[0]
+        
+        for tt in P:
+                for agind,ag in enumerate(tt[1]):
+                        #print(ag)
+                        st = I[agind]
+                        if mtickets[tt[0][agind]]==0:
+                                print(tt)
+                                print('no more tickets')
+                                return False
+                        else:
+                                mtickets[tt[0][agind]] -= 1
+                                
+                                if [tt[0][agind],ag] in U[st]:
+                                        I[agind] = ag
+                                        #pass
+                                else:
+                                        print(tt,agind)
+                                        print('invalid action')
+                                        return False
+                if(len(set(I))<3) and len(I)==3:
+                        print(tt)
+                        print('there is more than one police in the same location')
+                        return False
+        print(oP)
+        return True
+
 distance = lambda t1, t2 : math.sqrt((t2[0] - t1[0])**2 + (t2[1] - t1[1])**2)
 
 with open("coords.pickle", "rb") as fp:   # Unpickling
@@ -178,11 +223,20 @@ tinit = time.process_time()
 averageCost = calcAverageCostTransp(coords, M)
 print(averageCost)
 
+I = [28]
+
 #Taxi = linhas brancas; Autocarro = linhas azuis; Metro = linhas vermelhas
 SP = SearchProblem([85], M, coords)
-SOL = SP.search([28], tickets = [5, 5, 2])
+SOL = SP.search(I, tickets = [5, 5, 2])
 print(SOL)
 
 tend = time.process_time()
+
+if validatepath(SOL, I, M):
+    print("path")
+    print(SOL)
+    plotpath(SOL, coords)
+else:
+    print("invalid path")
 
 print(tend - tinit)
