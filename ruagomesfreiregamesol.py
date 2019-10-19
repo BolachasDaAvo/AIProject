afedd1fcba24b6fd724c2d8ec57b457506d133b9
BolachasDaAvo <miguelscfneves@tecnico.ulisboa.pt>
2019-10-19 14:34:20 +0100

    yes

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -32,14 +32,13 @@ class SearchProblem:
         depth = limitdepth
         g = 0
         h = 0
-        d = 0
         goal = self.goal[0]
         baseNode = Node(0, 0, 0, 0, [])
+        lastExpanded = 0
 
         for n in init:
             g = 0
-            d = distance(self.auxheur[n - 1], self.auxheur[goal - 1])
-            h = 1
+            h = 0 
             startNode = Node(n, baseNode, 0, h, tickets) 
             openNodes += [startNode]
 
@@ -51,6 +50,24 @@ class SearchProblem:
                 if x.f < q.f:
                     q = x
             openNodes.remove(q)
+            closedNodes += [q]
+
+            #We reached the goal
+            if q.n == goal:
+
+                #Trace path
+                node = q
+                while node.n != 0:
+                    sol += [node.n]
+                    node = node.p
+                sol.reverse()
+                print("foram gerados %d nos" % len(openNodes))
+                return sol
+
+            exp -= 1
+            if exp < 0:
+                print("limite de expansoes atingido")
+                return []
 
             for x in self.model[q.n]:
 
@@ -59,68 +76,111 @@ class SearchProblem:
 
                 #Node is parent node
                 if n == q.p.n:
-                    continue
-
-                if n == goal:
-
-                    #Trace path
-                    sol += [n]
-                    node = q
-                    while node.n != 0:
-                        sol += [node.n]
-                        node = node.p
-                    sol.reverse()
-                    return sol
-
-                g = q.g + 1
-                h = distance(self.auxheur[n - 1], self.auxheur[goal - 1]) / d
-                newNode = Node(n, q, g, h, [*(q.tickets)])
+                   continue
 
                 #We dont have enough tickets to travel
-                if newNode.remTicket(transp) < 0:
+                if tickets[transp] < 1:
                     continue
+
+                tickets = [*(q.tickets)]
+                tickets[transp] -= 1
+
+                g = q.g + 1
+
+                #Discovers what type of station it is
+                station = 0
+                for x in self.model[n]:
+                    if x[0] > station and tickets[x[0]] > 1:
+                        station = x[0]
+
+                h = distance(self.auxheur[n - 1], self.auxheur[goal - 1]) / averageCost[station]
+
+                newNode = Node(n, q, g, h, tickets)
 
                 #Check if node is already in openNodes list and has a lower f
                 flag = 0
                 for x in openNodes:
-                    if x.n == newNode.n and x.tickets == newNode.tickets and x.f < newNode.f:
-                        flag = 1
-                        break
+                    if x.n == newNode.n and x.f < newNode.f:
+                        if x.tickets[0] <= newNode.tickets[0] and x.tickets[1] <= newNode.tickets[1] and x.tickets[2] <= newNode.tickets[2]:
+                            flag = 1
+                            break
                 if (flag):
+                    print("skipped node %d" % newNode.n)
                     continue
 
-                #Check if node is in closed list and and has a lower f
+                #Check if node is in closed list and has a lower f
                 flag = 0
                 for x in closedNodes:
-                    if x.n == newNode.n and x.tickets == newNode.tickets and x.f < newNode.f:
-                        flag = 1
-                        break
+                    if x.n == newNode.n and x.f < newNode.f:
+                        if x.tickets[0] <= newNode.tickets[0] and x.tickets[1] <= newNode.tickets[1] and x.tickets[2] <= newNode.tickets[2]:
+                            flag = 1
+                            break
                 if (flag):
+                    print("skipped node %d" % newNode.n)
                     continue
                 openNodes += [newNode]
-                exp -= 1
-                if exp < 0:
-                    return []
-
-
-            closedNodes += [q]
 
         return []
+
+def calcAverageCost(coords, M):
+
+    edges = 0
+    totalSum = 0
+
+    processedEdges = []
+
+    vertices = len(M)
+
+    for i in range(1, vertices):
+        for x in M[i]:
+            if ((i, x[1]) or (x[1], i)) in processedEdges:
+                continue
+            processedEdges += (i, x[1])
+            processedEdges += (x[1], i)
+            edges += 1
+            totalSum += distance(coords[i - 1], coords[x[1] - 1])
+
+    return totalSum/edges
+
+def calcAverageCostTransp(coords, map):
+
+    transpNum = [0, 0, 0]
+    transpSum = [0, 0, 0]
+    averageCost = [0, 0, 0]
+
+    vertices = len(map)
+
+    for i in range(1, vertices):
+
+        for x in map[i]:
+            
+            transp = x[0]
+            vertex = x[1]
+            transpNum[transp] += 1
+            transpSum[transp] += distance(coords[i - 1], coords[vertex -1])
+
+    for i in range(3):
+        averageCost[i] = transpSum[i]/transpNum[i]
+
+    return averageCost
 
 distance = lambda t1, t2 : math.sqrt((t2[0] - t1[0])**2 + (t2[1] - t1[1])**2)
 
 with open("coords.pickle", "rb") as fp:   # Unpickling
     coords = pickle.load(fp)
 
-with open("mapasgraph.pickle", "rb") as fp:  # Unpickling
+with open("mapasgraph2.pickle", "rb") as fp:  # Unpickling
     AA = pickle.load(fp)
 M = AA[1]
 
 tinit = time.process_time()
 
+averageCost = calcAverageCostTransp(coords, M)
+print(averageCost)
+
 #Taxi = linhas brancas; Autocarro = linhas azuis; Metro = linhas vermelhas
-SP = SearchProblem([113], M, coords)
-SOL = SP.search([18], tickets = [5, 5, 2])
+SP = SearchProblem([85], M, coords)
+SOL = SP.search([28], tickets = [5, 5, 2])
 print(SOL)
 
 tend = time.process_time()
